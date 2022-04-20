38c9d62b5c3fcdd8f84ba2519553c5db1bb3bd15
Zhengyang Song <songzy_thu@163.com>
2022-04-20 09:28:33 +0800

    init skeleton to iterate on later.

---
+++ run.sh
@@ -0,0 +1,9 @@
+python train.py \
+    --num_layers 2 \
+    --hidden_size 512 \
+    --batch_size 128 \
+    --dropout 0.2 \
+    --init_scale  0.1 \
+    --max_grad_norm 5.0 \
+    --device gpu \
+    --model_path ./models
