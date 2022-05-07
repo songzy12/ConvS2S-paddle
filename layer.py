584d52f0961db5b043e8bac96a77ba0a33a2305b
Zhengyang Song <songzy_thu@163.com>
2022-05-07 20:08:51 +0800

    Add a reference to the importance of position embedding.

--- layer.py
+++ layer.py
@@ -1,6 +1,8 @@
 # TODO(songzy): take a look at the positional embedding:
 # - https://github.com/PaddlePaddle/PaddleNLP/blob/fa8fe0ef257a48b6517b01b177513391a6dfa8c2/paddlenlp/transformers/transformer/modeling.py#L786
 # - https://github.com/pytorch/fairseq/blob/51478ad3a19feed51d4bc4df5416870b7cee5347/fairseq/models/fconv.py#L243
+#
+# For effectiveness of position embeddings, see Section 5.4 of the ConvS2S paper.
 
 # TODO(songzy): take a look at ConvTBC and LinearizedConvolution:
 # - torch/nn/modules/conv.py
